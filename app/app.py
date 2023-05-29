1d0590f691020d595077541a7271a96df18f5813
Ayush Raina <ayushraina@iisc.ac.in>
2023-05-30 01:57:15 +0530

    added first page after start

--- app/app.py
+++ app/app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 import pandas as pd
 import numpy as np
 app = Flask(__name__ )
 
+#declaring the global variable for dataset
+@app.before_request
+def before_request():
+    g.dataset = None;
+
+#home page
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("HTML/index.html")
+
+#first page for uploading the dataset
+@app.route("/page1")
+def page1():
+    return render_template("HTML/firstpage.html")
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
--- app/static/js/redirect.js
+++ app/static/js/redirect.js
@@ -1,3 +1,3 @@
 function redirect() {
-    window.open("http://google.com", "_blank")
+    window.open("/page1", "_blank")
 }
