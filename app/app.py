9c577154f3d7cc6bd2b199aaccf40f7c42ea419e
Ayush Raina <ayushraina@iisc.ac.in>
2023-06-17 03:48:26 +0530

    added insights1 and 2

--- app/app.py
+++ app/app.py
@@ -23,7 +23,7 @@ def upload():
         file = request.files["file"]
         if file:
             df = pd.read_csv(file)
-            time.sleep(5)
+            # time.sleep(5)
             return render_template("uploadsuccess.html", message = "Dataset Uploaded Successfully")
         else:
             return render_template("firstpage.html", message = "Dataset not uploaded")
@@ -46,29 +46,90 @@ def page2():
     else:
         return render_template("dataset.html", dataset = "Dataset not uploaded")
 
-# #This will display size of uploaded dataset
-# @app.route("/shape")
-# def shape():
-#     global rc
-#     rc = df.shape
-#     rows = rc[0]
-#     columns = rc[1]
-#     rc = f"Rows: {rows} Columns: {columns}"
-#     return render_template("main_1.html",shape = rc)
+#page for displaying insightsI
+@app.route("/insights1")
+def insightsI():
+    #column names
+    cols = list(df.columns)
+    
+    # Row 1:- no. of missing values
+    misses = df.isnull().sum()
+    
+    # Row 2:- no. of present values
+    present = list(df.count())
+    
+    # Row 3:- type of data
+    dtypes = list(df.dtypes.astype("str"))
+    
+    # Row 4:- no. of unique values
+    unique = list(df.nunique())
+    
+    row1 = pd.DataFrame([present], columns=cols)
+    row2 = pd.DataFrame([misses], columns=cols)
+    row3 = pd.DataFrame([dtypes], columns=cols)
+    row4 = pd.DataFrame([unique], columns=cols)
+   
+    df_req = pd.concat([row1, row2, row3,row4], keys = ["Counts","Missing Values","Type","Unique Values"])
+    df_req = df_req.droplevel(1)
+    return render_template("/fundamentals/insightsI.html", dataset = df_req.to_html())
+   
+#page for displaying insights2
+@app.route("/insights2")
+def insightsII():
+    cols = list(df.columns)
+    
+    # Row 1:- minimum values
+    rep = "NC"
+    min_val = list(df.min())
+    min_val = [rep if type(x) == "str" else x for x in min_val]
 
-# #This will display the missing values in the dataset
-# @app.route("/miss")
-# def miss():
-#     global miss_values
-#     values = list(df.isnull().sum())
-#     miss_values = []
-#     arr = list(df.columns)
-#     for i in range(len(values)):
-#         if values[i] != 0:
-#             miss_values.append((arr[i], values[i])) # to get a list of tuples of feature and number of missing values
-        
-#     return render_template("main_1.html", missing_values = miss_values, shape = rc)
+    # Row 2:- maximum values
+    max_val = list(df.max())
+    max_val = [rep if type(x) == "str" else x for x in max_val]
 
+    # Calculation for further steps
+    df_copy = df
+    array = []
+    a = list(df.dtypes.astype("str"))
+    c = list(df.columns)
+    for i in range(len(a)):
+        if a[i] == "object":
+            change = c[i]
+            array.append(i)
+            df_copy[change] = -1
 
+    # Row 3:- Difference in min and max values
+    diff = df.max() - df.min()
+
+    # Row 4:- Mean of the values
+    mean = df_copy.mean()
+
+    # Row 5:- Median of the values
+    median = df_copy.median()
+
+    # Row 6:- Mean/Median difference
+    diff2 = abs(df.median() - df.mean())
+
+    # Row 7:- Standard Deviation of the values
+    std = df_copy.std()
+
+    row1 = pd.DataFrame([min_val], columns=cols)
+    row2 = pd.DataFrame([max_val], columns=cols)
+    row3 = pd.DataFrame([diff], columns=cols)
+    row4 = pd.DataFrame([mean], columns=cols)
+    row5 = pd.DataFrame([median], columns=cols)
+    row6 = pd.DataFrame([diff2], columns=cols)
+    row7 = pd.DataFrame([std], columns=cols)
+
+    df_req = pd.concat([row1, row2, row3,row4, row5, row6, row7], keys = ["Minimum","Maximum","Difference","Mean","Median","Mean-Median Difference","Standard Deviation"])
+    df_req = df_req.droplevel(1)
+
+    for i in range(len(array)):
+        index = array[i]
+        change = c[index]
+        df_req[change] = "NC"
+    
+    return render_template("/fundamentals/insightsII.html", dataset = df_req.to_html())
+    
 if __name__=="__main__":
     app.run(host="0.0.0.0")
--- app/static/js/redirect.js
+++ app/static/js/redirect.js
@@ -1,6 +1,6 @@
 // redirecting to page where we will upload dataset
 function redirect() {
-    window.location.href = "/page1";
+    window.location.href = "/main";
 }
 
 // redirecting to page where we will display dataset
@@ -10,26 +10,26 @@ function show_dataset() {
 
 // redirecting to main page
 function reachmain() {
-    window.open("/main", "_blank");
+    window.open("/main_1", "_blank");
 }
 
 // redirecting to main page with features
 function show_main1() {
-    window.location.href = "/main_1";
+    window.location.href = "/page1";
 }
 
 // By this we will get insights level 1 of dataset
 function insights1() {
-    window.location.href = "/insights1";
+    window.open("/insights1", "_blank");
 }
 
 // By this we will get insights level 2 of dataset
 function insights2() {
-    window.location.href = "/insights2";
+    window.open("/insights2", "_blank");
 }
 
 // By this we will get insights level 3 of dataset
 function insights3() {
-    window.location.href = "/insights3";
+    window.open("/insights3", "_blank");
 }
 
